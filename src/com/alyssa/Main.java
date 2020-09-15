8cbdcebb4f41fc03519cfc650f0a10a8cefcd01b
hendeak123 <alyssa.henderson@svu.edu>
2020-09-15 18:12:47 -0400

    Initial commit

---
+++ src/com/alyssa/Main.java
@@ -0,0 +1,16 @@
+package com.alyssa;
+
+class VolumeOfRectangularPrism {
+
+    public static void main() {
+
+        int height = 1;
+        int weight = 3;
+        int length = 5;
+
+        int volume = height * weight * length;
+
+        System.out.println("The volume of the prism is" + volume + ".");
+    }
+}
+
